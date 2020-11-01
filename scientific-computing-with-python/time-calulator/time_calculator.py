48f2028cc45a6ac9ce328ea9beb01f8f0f3b6961
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2020-11-01 20:47:25 +0100

    Add function converting AM/PM to 24 time

--- scientific-computing-with-python/time-calulator/time_calculator.py
+++ scientific-computing-with-python/time-calulator/time_calculator.py
@@ -1,7 +1,6 @@
-def add_time(start, duration):
-
-
-
-
-
-    return new_time
+def convert_to24(time):
+    hour = int(time[:2]) % 12
+    minute = int(time[3:5])
+    if "PM" in time:
+        hour += 12
+    return hour, minute
