496c34ab66d4295e68924588b5edb1f96fb1f86a
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2020-11-01 20:00:57 +0100

    Add arithmetic_arranger function

--- scientific-computing-with-python/arithmetic-formatter/arithmetic_arranger.py
+++ scientific-computing-with-python/arithmetic-formatter/arithmetic_arranger.py
@@ -1,4 +1,27 @@
 MAX_DIGITS = 4
+SPACING = "    "
+
+
+def arithmetic_arranger(problems):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    parsed_problems = []
+    for problem in problems:
+        try:
+            parsed_problems.append(parse_problem(problem))
+        except ValueError as e:
+            return str(e)
+
+    arranged_problems = ""
+    # add the three lines in arranged_problems
+    for i in range(0, 3):
+        for problem in parsed_problems:
+            arranged_problems += ''.join((problem[i], SPACING))
+        if i == 2:
+            break  # The last line doesn't need a newline character
+        arranged_problems = ''.join((arranged_problems.rstrip(), '\n'))
+    return arranged_problems
 
 
 def parse_problem(problem_string):
@@ -19,4 +42,4 @@ def parse_problem(problem_string):
     length = 2 + max(len(operand1), len(operand2))
     return [' ' * (length - len(operand1)) + operand1,
             split_string + ' ' * (length - len(operand2) - 1) + operand2,
-            length]
+            '-' * length]
