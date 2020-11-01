13d9add52365de6b58d3faa05050265d54bec2b7
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2020-11-01 21:02:58 +0100

    Add convert_to_12

--- scientific-computing-with-python/time-calulator/time_calculator.py
+++ scientific-computing-with-python/time-calulator/time_calculator.py
@@ -1,6 +1,14 @@
-def convert_to24(time):
+def convert_to_24(time):
     hour = int(time[:2]) % 12
     minute = int(time[3:5])
     if "PM" in time:
         hour += 12
     return hour, minute
+
+
+def convert_to_12(hours, minutes):
+    if hours < 12:
+        period = " AM"
+    else:
+        period = " PM"
+    return ''.join((str(hours % 12), ':', str(minutes), period))
