5803c1208ecac2949df2eaffa3a5e017be89a4f8
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2020-11-01 19:26:17 +0100

    Add parse_problem function to arithmetic_arranger

--- scientific-computing-with-python/arithmetic-formatter/arithmetic_arranger.py
+++ scientific-computing-with-python/arithmetic-formatter/arithmetic_arranger.py
@@ -1,4 +1,19 @@
-def arithmetic_arranger(problems):
+MAX_DIGITS = 4
 
 
-    return arranged_problems
+def parse_problem(problem_string):
+    if '+' in problem_string:
+        split_string = "+"
+    elif '-' in problem_string:
+        split_string = "-"
+    else:
+        raise ValueError("Error: Operator must be '+' or '-'.")
+
+    try:
+        operand1, operand2 = map(int, problem_string.split(split_string, 1))
+    except ValueError:
+        raise ValueError("Error: Numbers must only contain digits.")
+
+    if max(operand1, operand2) >= 10 ** MAX_DIGITS:
+        raise ValueError("Error: Numbers cannot be more than four digits.")
+    return operand1, operand2, split_string
