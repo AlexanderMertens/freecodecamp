41c73dacce5a4d83be2ef23882ad871c51d91de7
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2020-11-01 21:31:06 +0100

    Correct 24h to 12h clock conversions

--- scientific-computing-with-python/time-calulator/time_calculator.py
+++ scientific-computing-with-python/time-calulator/time_calculator.py
@@ -9,6 +9,20 @@ def convert_to_24(time):
 def convert_to_12(hours, minutes):
     if hours < 12:
         period = " AM"
+        if hours == 0:
+            hours = 12
     else:
         period = " PM"
-    return ''.join((str(hours % 12), ':', str(minutes), period))
+        hours -= 12
+    hours, minutes = num_to_string(hours, minutes)
+    return ''.join((hours, ':', minutes, period))
+
+
+def num_to_string(*nums):
+    result = []
+    for num in nums:
+        if num < 10:
+            result.append(''.join(('0', str(num))))
+        else:
+            result.append(str(num))
+    return result
