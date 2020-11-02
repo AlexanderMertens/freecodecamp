2d44eca7192da9d0a1b996eebd8860be7999f7cc
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2020-11-02 12:16:16 +0100

    Add Hat class
    The Hat Class models a hat containing an amount of coloured balls.
    From the hat a certain amount of balls can be randomly drawn
    without replacement.

--- scientific-computing-with-python/probability-calculator/prob_calculator.py
+++ scientific-computing-with-python/probability-calculator/prob_calculator.py
@@ -3,6 +3,19 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **balls):
+        self.contents = []
+        for ball_name, amount in balls.items():
+            self.add_balls(ball_name, amount)
+
+    def add_balls(self, name, amount):
+        self.contents += [name] * amount
+
+    def draw(self, amount):
+        drawn = random.sample(self.contents, k=amount)
+        for ball in drawn:
+            self.contents.remove(ball)
+        return drawn
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
